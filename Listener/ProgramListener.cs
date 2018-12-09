ba353406239b0601c29a2a5b9917fd73d95bd341
Shefali Lachhar <shefali.lachhar@gmail.com>
2018-12-09 09:24:18 +0530

    multiple delegate example a source and listener

---
+++ Listener/ProgramListener.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Listener
+{
+    public class ProgramListener
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am Listener");
+            Console.ReadLine();
+        }
+
+        public static void FunctionForDelegate1(String name)
+        {
+            Console.WriteLine("This is first delegate :" + name);
+        }
+
+        public static void FunctionForDelegate2(String name)
+        {
+            Console.WriteLine("This is 2nd delegate : " + name);
+        }
+
+        public static void FunctionForDelegate3(String name)
+        {
+            Console.WriteLine("This is 2nd delegate : " + name);
+        }
+    }
+}
---
+++ Source/Program.cs
@@ -0,0 +1,25 @@
+﻿using Listener;
+using System;
+
+namespace Source
+{
+    internal class Program
+    {
+        public delegate void FirstDelegate(string name);
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, I am source!");
+
+            FirstDelegate del1 = new FirstDelegate(ProgramListener.FunctionForDelegate1);
+            FirstDelegate del2 = new FirstDelegate(ProgramListener.FunctionForDelegate2);
+            FirstDelegate del3 = new FirstDelegate(ProgramListener.FunctionForDelegate3);
+
+            del1 = del1 + del2 + del3;
+            del1("Shefali");
+            Console.ReadLine();
+        }
+        
+
+    }
+}
