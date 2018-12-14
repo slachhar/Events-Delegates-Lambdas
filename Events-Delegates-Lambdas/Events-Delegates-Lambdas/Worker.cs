f72b777ca029a78e979bd1572c589350eab3d44e
slachhar <shefali.lachhar@gmail.com>
2018-12-14 12:02:28 +0530

    Delegate EventArgs Class

---
+++ Events-Delegates-Lambdas/Events-Delegates-Lambdas/Worker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Source
+{
+    //public delegate int WorkPerformedHandler(object sender, WorkPerformedEventArgs e);
+
+    public class Worker
+    {
+        public event EventHandler<WorkPerformedEventArgs> WorkPerformed;
+        public event EventHandler WorkCompleted;
+
+        public void DoWork(int hours, WorkType workType)
+        {
+            for (int i = 0; i < hours; i++)
+            {
+				Thread.Sleep(1000);
+                OnWorkPerformed(i+1, workType);
+            }
+            OnWorkCompleted();
+        }
+
+        protected virtual void OnWorkPerformed(int hours, WorkType workType)
+        {
+            //if(WorkPerformed != null)
+            //{
+            //    WorkPerformed(hours,workType);
+            //}
+
+            var del = WorkPerformed as EventHandler<WorkPerformedEventArgs>;
+            if(del != null)
+            {
+                del(this, new WorkPerformedEventArgs(hours, workType));
+            }
+        }
+
+        protected virtual void OnWorkCompleted()
+        {
+            //if(WorkPerformed != null)
+            //{
+            //    WorkPerformed(hours,workType);
+            //}
+
+            var del = WorkCompleted as EventHandler;
+            if (del != null)
+            {
+                del(this, EventArgs.Empty);
+            }
+        }
+    }
+}
